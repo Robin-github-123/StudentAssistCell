87f85f78ab53de220697f208d2f99d911945920b
josephsaviokav <josephsaviokav@gmail.com>
2025-09-03 19:36:34 +0530

    first commit

---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//sample new page creation
+
+export default function AboutPage() {
+  return (
+    <div>This is the about page</div>
+  )
+}
---
+++ client/src/app/minor/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function MinorPage() {
+  return (
+    <div>This is the minor page</div>
+  )
+}
+
